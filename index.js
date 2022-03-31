61a31f1eaeb29299dc029acc9417bea97c5e9320
Philip Levy <philipdlevy@gmail.com>
2022-03-31 10:39:55 -0600

    added gitignore

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("content has loaded")
+
+
+let submitForm = document.querySelector("#comment-form")
+submitForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+const list = document.querySelector("#list")
+const li = document.createElement("li")
+li.innerText = event.target.comment.value
+list.appendChild(li)
+    })
 })
 
 
